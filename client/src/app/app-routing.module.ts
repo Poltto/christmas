352cad6cbaeaaa9bc335050ca40fa60c8f6105f4
Poltto <martti.kaipiainen@gmail.com>
2022-11-30 18:18:47 +0100

    Added wine route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuardService } from './auth-guard.service';
+import { WineComponent } from './wine/wine-component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'main', component: MainComponent, canActivate: [AuthGuardService] },
+  { path: 'wine', component: WineComponent, canActivate: [AuthGuardService] },
 ];
 
 @NgModule({
